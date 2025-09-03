e1beb5b9147cba1e1f29ebad880bd17bb31a0512
bebusl <bebus1998@naver.com>
2025-09-03 17:24:54 +0900

    chore: [FN-159]tanstack-router 설치 및 vite 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -5,7 +6,14 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

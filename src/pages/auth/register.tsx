9954f0283e8bf9d75c83ba4ae1d842ba4ce073c5
bebusl <bebus1998@naver.com>
2025-09-03 17:21:12 +0900

    fix: 회원가입 폼 id와 for 불일치 해결

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -9,7 +9,7 @@ import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/button";
 import { Label } from "../../components/ui/label";
 import { Checkbox } from "../../components/ui/checkbox";
-import { Separator } from "../../components/ui/separator";
+// import { Link } from "@tanstack/react-router";
 
 // type Props = {};
 const Register = () => {
@@ -20,41 +20,38 @@ const Register = () => {
       </CardHeader>
       <CardContent>
         <Label htmlFor="email">이메일</Label>
-        <Input id="email"></Input>
+        <Input id="email" type="email" name="email" autoComplete="off"></Input>
         <Button>인증하기</Button>
-        <Input id="email" placeholder="인증코드"></Input>
+        <Input id="email-verify" placeholder="인증코드"></Input>
         <Button>제출하기</Button>
         <Label htmlFor="password">비밀번호</Label>
         <Input type="password" id="password"></Input>
-        <Label htmlFor="password">비밀번호 확인</Label>
-        <Input type="password" id="password"></Input>
+        <Label htmlFor="password-verify">비밀번호 확인</Label>
+        <Input
+          type="password"
+          name="password-verify"
+          id="password-verify"
+        ></Input>
         <Label htmlFor="nickname">닉네임</Label>
-        <Input id="nickname"></Input>
-        <Label htmlFor="phone">휴대폰번호 확인</Label>
-        <Input type="phone" id="phone"></Input> <Button>인증하기</Button>
-        <Input id="email" placeholder="인증코드"></Input>
-        <Button>제출하기</Button>
+        <Input id="nickname" name="nickname"></Input>
+        <Label htmlFor="phone">휴대폰번호</Label>
+        <Input type="phone" id="phone"></Input>
         <Button>인증하기</Button>
-        <Input id="email" placeholder="인증코드"></Input>
+        <Label htmlFor="phone-verify">휴대폰번호 확인</Label>
+        <Input
+          id="phone-verify"
+          name="phone-verify"
+          placeholder="인증코드"
+        ></Input>
         <Button>제출하기</Button>
+
         <Checkbox id="accept" />
         <Label htmlFor="accept">SMS 수신여부 동의</Label>
         {/* <Link to="/reset-password">비밀번호 찾기</Link> */}
       </CardContent>
       <CardFooter className="flex-col gap-2">
         <Button type="submit" variant="default">
-          로그인
-        </Button>
-        <Separator />
-        <p>또는 소셜 계정으로 회원가입</p>
-        <Button variant="outline" className="w-full">
-          Register with Google
-        </Button>
-        <Button variant="outline" className="w-full">
-          Register with Kakao
-        </Button>
-        <Button variant="outline" className="w-full">
-          Register with Github
+          회원가입
         </Button>
       </CardFooter>
     </Card>

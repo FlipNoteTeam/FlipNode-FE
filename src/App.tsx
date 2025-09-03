ce6bc815f8b85242e3f473e6fad0ecfae97e36ea
bebusl <bebus1998@naver.com>
2025-09-03 17:25:14 +0900

    feat: [FN-159]기본 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function App() {
-  return <>qhqhqh</>;
+  return (
+    <div className="min-w-dvw min-h-dvh p-8 bg-gray-50">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
